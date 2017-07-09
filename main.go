a586c36ba27d948cda4d6d265852302dd13ffa06
Ravi Chodavarapu <rchodava@gmail.com>
2017-07-09 01:57:20 +0100

    Setup gradle-based build for Windows C++ build Document how to build POC for minikube-based CLI

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import "k8s.io/minikube/pkg/minikube/machine"
+import "k8s.io/minikube/cmd/minikube/cmd"
+import "k8s.io/minikube/pkg/minikube/config"
+import "github.com/spf13/viper"
+import "github.com/spf13/cobra"
+import "time"
+
+func initConfig() {
+        viper.Set(config.WantKubectlDownloadMsg, false)
+        viper.Set(config.WantUpdateNotification, false)
+}
+
+func main() {
+        cobra.OnInitialize(initConfig)
+
+        machine.StartDriver()
+        cmd.RootCmd.SetArgs([]string{"start"})
+        cmd.RootCmd.Execute()
+        time.Sleep(10 * time.Second)
+        cmd.RootCmd.SetArgs([]string{"dashboard"})
+        cmd.RootCmd.Execute()
+}
