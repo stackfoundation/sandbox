6950e9c9e539afd797772aaf2c8fc68877365eb9
Ravi Chodavarapu <rchodava@gmail.com>
2017-07-20 17:58:14 +0100

    Stream logs as part of exec

--- src/github.com/stackfoundation/core/cmd/exec.go
+++ src/github.com/stackfoundation/core/cmd/exec.go
@@ -1,81 +1,100 @@
 package cmd
 
 import (
-	"fmt"
-	"os"
+        "fmt"
+        "os"
 
-	"github.com/golang/glog"
-	"github.com/spf13/cobra"
-	"github.com/stackfoundation/core/pkg/minikube/config"
-	"github.com/stackfoundation/core/pkg/minikube/constants"
-	"github.com/stackfoundation/core/pkg/minikube/machine"
-	"github.com/stackfoundation/core/pkg/util/kubeconfig"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/pkg/api/v1"
-	"k8s.io/client-go/tools/clientcmd"
+        "github.com/golang/glog"
+        "github.com/spf13/cobra"
+        "github.com/stackfoundation/core/pkg/minikube/config"
+        "github.com/stackfoundation/core/pkg/minikube/constants"
+        "github.com/stackfoundation/core/pkg/minikube/machine"
+        "github.com/stackfoundation/core/pkg/util/kubeconfig"
+        metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+        "k8s.io/client-go/kubernetes"
+        "k8s.io/client-go/pkg/api/v1"
+        "k8s.io/client-go/tools/clientcmd"
+        "io"
+        "time"
 )
 
 var execImage string
 var execCommandString string
 
 var execCmd = &cobra.Command{
-	Use:   "exec",
-	Short: "Execute a single command within a Docker container",
-	Long:  `Execute a single command within a Docker container.`,
-	Run: func(command *cobra.Command, args []string) {
-		startKube()
+        Use:   "exec",
+        Short: "Execute a single command within a Docker container",
+        Long:  `Execute a single command within a Docker container.`,
+        Run: func(command *cobra.Command, args []string) {
+                startKube()
 
-		api, err := machine.NewAPIClient()
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error getting client: %s\n", err)
-			os.Exit(1)
-		}
-		defer api.Close()
+                api, err := machine.NewAPIClient()
+                if err != nil {
+                        fmt.Fprintf(os.Stderr, "Error getting client: %s\n", err)
+                        os.Exit(1)
+                }
+                defer api.Close()
 
-		con, err := kubeconfig.ReadConfigOrNew(constants.KubeconfigPath)
-		if err != nil {
-			glog.Errorln("Error kubeconfig status:", err)
-			MaybeReportErrorAndExit(err)
-		}
+                con, err := kubeconfig.ReadConfigOrNew(constants.KubeconfigPath)
+                if err != nil {
+                        glog.Errorln("Error kubeconfig status:", err)
+                        MaybeReportErrorAndExit(err)
+                }
 
-		configOverrides := &clientcmd.ConfigOverrides{}
-		k8sClientConfig := clientcmd.NewNonInteractiveClientConfig(*con, config.GetMachineName(), configOverrides, nil)
-		restClient, err := k8sClientConfig.ClientConfig()
-		clientset, err := kubernetes.NewForConfig(restClient)
+                configOverrides := &clientcmd.ConfigOverrides{}
+                k8sClientConfig := clientcmd.NewNonInteractiveClientConfig(*con, config.GetMachineName(), configOverrides, nil)
+                restClient, err := k8sClientConfig.ClientConfig()
+                clientset, err := kubernetes.NewForConfig(restClient)
 
-		pods := clientset.Pods("default")
-		pod, err := pods.Create(&v1.Pod{
-			ObjectMeta: metav1.ObjectMeta{
-				GenerateName: "sbox-",
-			},
-			Spec: v1.PodSpec{
-				Containers: []v1.Container{
-					v1.Container{
-						Name:    "sboxstep",
-						Image:   execImage,
-						Command: []string{execCommandString},
-					},
-				},
-				RestartPolicy: v1.RestartPolicyNever,
-			},
-		})
-		if err != nil {
-			panic(err.Error())
-		}
-		fmt.Printf("Started a pod: %v!\n", pod.Name)
+                pods := clientset.Pods("default")
+                pod, err := pods.Create(&v1.Pod{
+                        ObjectMeta: metav1.ObjectMeta{
+                                GenerateName: "sbox-",
+                        },
+                        Spec: v1.PodSpec{
+                                Containers: []v1.Container{
+                                        v1.Container{
+                                                Name:    "sboxstep",
+                                                Image:   execImage,
+                                                Command: []string{execCommandString},
+                                        },
+                                },
+                                RestartPolicy: v1.RestartPolicyNever,
+                        },
+                })
+                if err != nil {
+                        panic(err.Error())
+                }
+                fmt.Printf("Started a pod: %v!\n", pod.Name)
 
-		response := pods.GetLogs(pod.Name, &v1.PodLogOptions{Follow: true}).Do()
-		if response.Error() != nil {
-			panic(response.Error())
-		}
+                for {
+                        podStatus, err := pods.Get(pod.Name, metav1.GetOptions{})
+                        if err != nil {
+                                break
+                        }
 
-	},
+                        if podStatus.Status.Phase == v1.PodRunning || podStatus.Status.Phase == v1.PodSucceeded ||
+                                podStatus.Status.Phase == v1.PodFailed {
+				break
+                        }
+
+                        time.Sleep(100 * time.Millisecond)
+                }
+
+                req := pods.GetLogs(pod.Name, &v1.PodLogOptions{Follow: true})
+                stream, err := req.Stream()
+                if err != nil {
+                        panic(err.Error())
+                }
+
+                defer stream.Close()
+                _, _ = io.Copy(os.Stdout, stream)
+        },
 }
 
 func init() {
-	configureKubeStartingCommandFlags(execCmd)
-	execCmd.Flags().StringVarP(&execImage, "image", "i", "", "Image to run")
-	execCmd.Flags().StringVarP(&execCommandString, "command", "c", "", "Command to run")
-	RootCmd.AddCommand(execCmd)
+        configureKubeStartingCommandFlags(execCmd)
+        execCmd.Flags().StringVarP(&execImage, "image", "i", "", "Image to run")
+        execCmd.Flags().StringVarP(&execCommandString, "command", "c", "", "Command to run")
+        RootCmd.AddCommand(execCmd)
 }
